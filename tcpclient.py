98bd1ce1abc8eeeedda0166b0f76c0d9259be7a0
KtokKawu <kawublack@gmail.com>
2018-05-07 01:21:28 +0900

    add new file

---
+++ tcpclient.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+#ソケットオブジェクト作成
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#サーバへ接続
+client.connect((target_host, target_port))
+
+#データ送信
+client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+#データ受信
+response = client.recv(4096)
+
+print response
+
